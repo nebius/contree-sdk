a115bf8041234aaf10def3085788f2bc65800704
Andrei Andriushchenko <delphikettle@gmail.com>
2025-11-18 12:49:39 +0100

    added wrapper

---
+++ contree-sdk/contree_sdk/utils/wrapper.py
@@ -0,0 +1,100 @@
+import asyncio
+from asyncio import AbstractEventLoop, Task
+from collections.abc import Awaitable, Callable
+from concurrent.futures import Future
+from functools import partial, wraps
+from threading import Event, Lock, Thread
+from typing import ParamSpec, TypeVar
+
+
+P = ParamSpec("P")
+T = TypeVar("T")
+
+
+def _on_task_done(future: Future, task: asyncio.Task):
+    if task.exception():
+        future.set_exception(task.exception())
+    else:
+        future.set_result(task.result())
+
+
+class _WrapperContext:
+    def __init__(self):
+        self.thread: Thread | None = None
+        self.tasks: set[Task] = set()
+        self._loop: AbstractEventLoop | None = None
+        self._lock: Lock = Lock()
+        self._start_event: Event = Event()
+
+    def ensure_running(self):
+        if self._loop is not None:
+            return
+        with self._lock:
+            self._start_event.clear()
+            self.thread = Thread(target=self._thread_main, daemon=True)
+            self.thread.start()
+            self._start_event.wait()
+
+    @property
+    def loop(self):
+        self.ensure_running()
+        return self._loop
+
+    def _thread_main(self):
+        self._loop = asyncio.new_event_loop()
+        self._start_event.set()
+        self.loop.run_forever()
+
+    def _close(self):
+        for task in self.tasks:
+            task.cancel()
+        self.tasks.clear()
+        with self._lock:
+            if self._loop is not None:
+                self._loop.close()
+                self._loop = None
+        self.thread = None
+
+    def close(self):
+        self._loop.call_soon_threadsafe(self._close)
+
+    def __del__(self):
+        self.close()
+
+
+class AsyncWrapper:
+    _shared_context: _WrapperContext = _WrapperContext()
+
+    def __init__(self, is_global: bool = True):
+        self._is_global = is_global
+        if self._is_global:
+            self._context = self._shared_context
+        else:
+            self._context = _WrapperContext()
+
+    def _start_coro(self, coro: Awaitable[T], future: Future) -> asyncio.Task:
+        task = self._context.loop.create_task(coro)
+        self._context.tasks.add(task)
+        task.add_done_callback(self._context.tasks.discard)
+        task.add_done_callback(partial(_on_task_done, future))
+        return task
+
+    def _coro_to_future(self, coro: Awaitable[T]) -> Future:
+        future = Future()
+        self._context.loop.call_soon_threadsafe(self._start_coro, coro, future)
+        return future
+
+    def decorator(self, func: Callable[P, Awaitable[T]]) -> Callable[P, T]:
+        @wraps(func)
+        def wrapper(*args: P.args, **kwargs: P.kwargs) -> T:
+            return self.wrap(func(*args, **kwargs))
+
+        return wrapper
+
+    def wrap(self, coro: Awaitable[T]) -> T:
+        return self._coro_to_future(coro).result()
+
+
+def wrap_async(func: Callable[P, Awaitable[T]]) -> Callable[P, T]:
+    wrapper = AsyncWrapper()
+    return wrapper.decorator(func)
---
+++ contree-sdk/tests/utils/test_wrapper.py
@@ -0,0 +1,92 @@
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime
+from threading import Thread
+
+import pytest
+
+from contree_sdk.utils.wrapper import wrap_async
+
+
+_wait_time = 0.01
+
+
+async def fake_task(a: int, b: int = 1, ex: Exception = None) -> int:
+    await asyncio.sleep(_wait_time)
+    if ex:
+        raise ex
+    return a + 2 * b
+
+
+fake_task_sync = wrap_async(fake_task)
+
+
+def test_basic():
+    res = fake_task_sync(3)
+    assert res == 5
+
+
+def test_exception():
+    with pytest.raises(RuntimeError):
+        fake_task_sync(3, ex=RuntimeError("Some error"))
+
+
+def test_multiple_threads():
+    data = {2, 3, 4, 5, 6}
+    threads = []
+
+    def run_task(a, b):
+        result = fake_task_sync(a, b)
+        data.discard(result)
+
+    started = datetime.now()
+    for i in range(5):
+        thread = Thread(target=run_task, args=(i, 1))
+        threads.append(thread)
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+    spent = datetime.now() - started
+    assert spent.total_seconds() <= _wait_time * 1.2
+
+    assert data == set()
+
+
+def test_thread_pool_executor():
+    with ThreadPoolExecutor(max_workers=3) as executor:
+        futures = [executor.submit(fake_task_sync, i, 2) for i in range(5)]
+        executor_results = [future.result() for future in futures]
+
+    assert len(executor_results) == 5
+    assert sorted(executor_results) == [4, 5, 6, 7, 8]
+
+
+def test_multiple_tasks_in_threads():
+    data = {2, 3, 4, 5, 6}
+    threads = []
+
+    def run_task(a, b):
+        result = fake_task_sync(a, b)
+        data.discard(result)
+
+    @wrap_async
+    async def another_task():
+        await asyncio.sleep(_wait_time / 2)
+
+    started = datetime.now()
+    for _ in range(50):
+        thread = Thread(target=another_task)
+        threads.append(thread)
+        thread.start()
+    for i in range(10):
+        thread = Thread(target=run_task, args=(i, 1))
+        threads.append(thread)
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+    spent = datetime.now() - started
+    assert spent.total_seconds() <= _wait_time * 1.5
+    assert data == set()
