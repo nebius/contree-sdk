7694282e17786950eacb34d4dc4a8e0ea2cbc8b6
Andrei Andriushchenko <delphikettle@gmail.com>
2025-11-21 17:37:31 +0100

    implemented io wrapping for different io support

---
+++ contree-sdk/contree_sdk/utils/io_wrap.py
@@ -0,0 +1,70 @@
+from enum import StrEnum
+from io import BytesIO, IOBase, StringIO
+from os import fdopen, pipe
+from pathlib import Path
+from subprocess import PIPE
+from typing import IO, AnyStr
+
+
+class PipeIO(IOBase):
+    def __init__(self) -> None:
+        super().__init__()
+        r, w = pipe()
+        self._r = fdopen(r, "rb", buffering=0)
+        self._w = fdopen(w, "wb", buffering=0)
+
+    def readable(self) -> bool:
+        return True
+
+    def writable(self) -> bool:
+        return True
+
+    def seekable(self) -> bool:
+        return False
+
+    def read(self, size: int = -1) -> bytes:
+        return self._r.read(size)
+
+    def readline(self, size: int = -1) -> bytes:
+        return self._r.readline(size)
+
+    def write(self, b: bytes) -> int:
+        return self._w.write(b)
+
+    def flush(self) -> None:
+        self._w.flush()
+
+    def close(self) -> None:
+        if not self._w.closed:
+            self._w.close()
+            return
+        self._r.close()
+
+    @property
+    def closed(self) -> bool:
+        return self._w.closed and self._r.closed
+
+
+class IOMode(StrEnum):
+    read = "r"
+    write = "w"
+
+
+def get_io_by_obj(obj: str | bytes | Path | IO[AnyStr] | PIPE | None, mode: IOMode) -> IOBase | IO | None:
+    if obj is None:
+        return None
+    if obj is PIPE:
+        return PipeIO()
+    if isinstance(obj, IOBase):
+        return obj
+    if isinstance(obj, bytes):
+        assert mode == IOMode.read
+        return BytesIO(obj)
+    if isinstance(obj, str):
+        if mode == IOMode.read:
+            return StringIO(obj)
+        obj = Path(obj)
+    if not isinstance(obj, Path):
+        raise TypeError(f"{obj} is not supported as IO")
+    file_mode = mode + "b"
+    return obj.open(file_mode)
---
+++ contree-sdk/tests/unit/io/test_io.py
@@ -0,0 +1,93 @@
+from io import BytesIO, StringIO
+from pathlib import Path
+from subprocess import PIPE
+from tempfile import NamedTemporaryFile
+
+import pytest
+
+from contree_sdk.utils.io_wrap import IOMode, get_io_by_obj
+
+
+@pytest.fixture
+def tmp_file() -> Path:
+    with NamedTemporaryFile() as f:
+        yield Path(f.name)
+
+
+def test_str_input():
+    string = "test data"
+    io_obj = get_io_by_obj(string, IOMode.read)
+    assert io_obj.read() == string
+
+
+def test_str_io_input():
+    string = "test data"
+    io_obj = get_io_by_obj(StringIO(string), IOMode.read)
+    assert io_obj.read() == string
+
+
+def test_bytes_input():
+    data = b"test data"
+    io_obj = get_io_by_obj(data, IOMode.read)
+    assert io_obj.read() == data
+
+
+def test_bytes_io_input():
+    data = b"test data"
+    io_obj = get_io_by_obj(BytesIO(data), IOMode.read)
+    assert io_obj.read() == data
+
+
+def test_path_input(test_txt_path: Path):
+    io_obj = get_io_by_obj(test_txt_path, IOMode.read)
+    assert io_obj.read() == test_txt_path.read_bytes()
+
+
+def test_pipe_input():
+    data = b"test data"
+    io_obj = get_io_by_obj(PIPE, IOMode.read)
+    io_obj.write(data)
+    io_obj.close()
+    assert io_obj.read() == data
+
+
+def test_path_output(tmp_file: Path):
+    data = b"test data"
+    io_obj = get_io_by_obj(tmp_file, IOMode.write)
+    io_obj.write(data)
+    io_obj.flush()
+    assert tmp_file.read_bytes() == data
+
+
+def test_str_path_output(tmp_file: Path):
+    data = b"test data"
+    io_obj = get_io_by_obj(str(tmp_file), IOMode.write)
+    io_obj.write(data)
+    io_obj.flush()
+    assert tmp_file.read_bytes() == data
+
+
+def test_bytes_io_output():
+    data = b"test data"
+    obj = BytesIO()
+    io_obj = get_io_by_obj(obj, IOMode.write)
+    io_obj.write(data)
+    io_obj.flush()
+    assert obj.getvalue() == data
+
+
+def test_str_io_output():
+    data = "test data"
+    obj = StringIO()
+    io_obj = get_io_by_obj(obj, IOMode.write)
+    io_obj.write(data)
+    io_obj.flush()
+    assert obj.getvalue() == data
+
+
+def test_pipe_output():
+    data = b"test data"
+    io_obj = get_io_by_obj(PIPE, IOMode.write)
+    io_obj.write(data)
+    io_obj.close()
+    assert io_obj.read() == data
