ec229890344562d20f4c8e4ccc3d659885b1a40a
Andrei Andriushchenko <delphikettle@gmail.com>
2026-01-23 14:33:20 +0100

    basedpyright fix after import

--- contree-sdk/tests/unit/utils/test_lib.py
+++ contree-sdk/tests/unit/utils/test_lib.py
@@ -36,4 +36,6 @@ async def test_read_timeout(fake_contree_config: ContreeConfig, httpbin):
     new_exp = wrap_api_exception(exc)
     assert isinstance(new_exp, ApiTimeoutError)
     assert new_exp.timeout_type == "read"
+    assert new_exp.response is not None
+    assert new_exp.response.headers is not None
     assert isinstance(new_exp.response.headers["content-type"], str)
